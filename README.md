25ebbc6c6975e31bf61388b6b97d39c3cf6038c0
Stephen Hineline <subasteve@gmail.com>
2015-03-14 16:00:14 -0600

    0.1.2: Add image resizing

--- README.md
+++ README.md
@@ -32,7 +32,13 @@ grunt.initConfig({
       ],
       dest: "output.file",
       prefix: "font_",
-      dimensions: false //obtain dimensions and add vars in less
+      dimensions: false, //[Optional] Obtain dimensions and add vars in less
+      resize: { //[Optional] Resize images
+      	imageMagick: true, //[Optional] defaults to GraphicsMagick if not set
+      	width: 100, //Use width alone to change width while keeping aspect ratio
+      	height: 100, //Use height alone to change height while keeping aspect ratio
+      	force: true //[Optional] Forces aspect ratio change
+      }
     }
   }
 })
@@ -42,6 +48,7 @@ grunt.initConfig({
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
 
 ## Release History
+0.1.2: Add image resizing 
 0.1.11: Fix version  
 0.1.1: Dimensions output added   
 0.1.0: Initial release
--- tasks/base64Less.js
+++ tasks/base64Less.js
@@ -12,8 +12,14 @@ module.exports = function(grunt) {
   
   var mime = require('mime'),
   fs = require('fs'),
-  sizer = require('image-size');
-  
+  sizer = require('image-size'),
+  gm = require('gm'),
+  errorResize = function errorResize(err) {
+  	if(!err){
+		grunt.log.writeln("errorResize",err);
+	}
+  };
+
   grunt.registerMultiTask('base64Less', 'Base64 encode files. Into less file format.', function() {
     var data = this.data,
     done = this.async(),
@@ -30,22 +36,50 @@ module.exports = function(grunt) {
         return false;
     }
     
+    if(data.resize.imageMagick){
+    	    gm = gm.subClass({ imageMagick: true });
+    }
+
     if(!filesToProcess.length){
 	grunt.log.writeln("Nothing to process in "+data.process);
 	done();
     }else{
 	grunt.log.writeln("Processing  "+filesToProcess.length+" files...");
 	for(var i = 0; i < filesToProcess.length; i++){
+		if(data.resize){
+			var dimensions = sizer(filesToProcess[i]);
+			if(data.resize.width && !data.resize.height){
+				if(dimensions.width !== data.resize.width){
+					gm(filesToProcess[i]).resize(data.resize.width).write(filesToProcess[i],errorResize);
+					grunt.log.writeln('Resizing file '+filesToProcess[i]+' to width '+data.resize.width);
+				}
+			}else if(!data.resize.width && data.resize.height){
+				if(dimensions.height !== data.resize.height){
+					gm(filesToProcess[i]).resize(null,data.resize.height).write(filesToProcess[i],errorResize);
+					grunt.log.writeln('Resizing file '+filesToProcess[i]+' to height '+data.resize.height);
+				}
+			}else if(data.resize.width && data.resize.height){
+				if(dimensions.width !== data.resize.width && dimensions.height !== data.resize.height){
+					if(data.resize.force){
+						gm(filesToProcess[i]).resize(data.resize.width,data.resize.height,"!").write(filesToProcess[i],errorResize);
+					}else{
+						gm(filesToProcess[i]).resize(data.resize.width,data.resize.height).write(filesToProcess[i],errorResize);
+					}
+					grunt.log.writeln('Resizing file '+filesToProcess[i]+' to width '+data.resize.width+' height '+data.resize.height+' force: '+data.resize.force);
+				}
+			}
+		}
 		var lessFileName = filesToProcess[i].substring(filesToProcess[i].lastIndexOf("/")+1),
 		lessSafeFileName = lessFileName.replace(".","_"),
 		contents = fs.readFileSync(filesToProcess[i]),
 		base64Contents = contents.toString('base64');
 		
 		output += "@"+data.prefix+lessSafeFileName+":\"data:"+mime.lookup(filesToProcess[i])+";base64,"+base64Contents+"\";\n";
-		if(data.size){
-			var dimensions = sizer(filesToProcess[i]);
-			output += "@"+data.prefix+lessSafeFileName+"_width: "+dimensions.width+"px;\n";
-			output += "@"+data.prefix+lessSafeFileName+"_height: "+dimensions.height+"px;\n";	
+	
+		if(data.dimensions){
+			var dimensions1 = sizer(filesToProcess[i]);
+			output += "@"+data.prefix+lessSafeFileName+"_width: "+dimensions1.width+"px;\n";
+			output += "@"+data.prefix+lessSafeFileName+"_height: "+dimensions1.height+"px;\n";	
 		}
 		grunt.log.writeln('Base64 encoded "' + filesToProcess[i] + '" to '+data.prefix+lessSafeFileName+' less entry.');    
 	}
