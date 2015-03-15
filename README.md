4a3190381b85678462eb2a4e2d66c96f0571103a
Stephen Hineline <subasteve@gmail.com>
2015-03-14 18:07:44 -0600

    0.2.0: Bug fixes, cleaned up

--- README.md
+++ README.md
@@ -48,7 +48,8 @@ grunt.initConfig({
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
 
 ## Release History
-0.1.2: Add image resizing 
+0.2.0: Bug fixes, cleaned up  
+0.1.2: Add image resizing   
 0.1.11: Fix version  
 0.1.1: Dimensions output added   
 0.1.0: Initial release
--- tasks/base64Less.js
+++ tasks/base64Less.js
@@ -6,7 +6,7 @@
  * Licensed under the Apache 2.0 licenses.
  */
 
-'use strict';
+//'use strict';
 
 module.exports = function(grunt) {
   
@@ -14,17 +14,44 @@ module.exports = function(grunt) {
   fs = require('fs'),
   sizer = require('image-size'),
   gm = require('gm'),
-  errorResize = function errorResize(err) {
-  	if(!err){
+  output = "",
+  pending = [],
+  completeTimer = null;
+
+  function encode(prefix,lessSafeFileName,fileName){
+   	var contents = fs.readFileSync(fileName),
+	    base64Contents = contents.toString('base64');
+	grunt.log.writeln('Base64 encoded "' + fileName + '" to '+prefix+lessSafeFileName+' less entry.');	
+	return "@"+prefix+lessSafeFileName+":\"data:"+mime.lookup(fileName)+";base64,"+base64Contents+"\";\n";
+  }
+
+  function writeDimensions(prefix,lessSafeFileName,fileName){
+    	var dimensions1 = sizer(fileName);
+	return "@"+prefix+lessSafeFileName+"_width: "+dimensions1.width+"px;\n@"+prefix+lessSafeFileName+"_height: "+dimensions1.height+"px;\n";
+  }
+
+  function create(prefix,lessSafeFileName,fileName,needDimensions){
+  	return (!needDimensions) ? encode(prefix,lessSafeFileName,fileName) : encode(prefix,lessSafeFileName,fileName)+writeDimensions(prefix,lessSafeFileName,fileName);
+  }
+
+
+  function errorResize(err,prefix,lessSafeFileName,fileName,needDimensions) {
+  	if(typeof err !== "undefined"){
 		grunt.log.writeln("errorResize",err);
 	}
-  };
+	output += create(prefix,lessSafeFileName,fileName,needDimensions);
+  	var index = pending.indexOf(lessSafeFileName);
+  	if(index > -1){
+		pending.splice(index, 1);
+	}
+  }
 
   grunt.registerMultiTask('base64Less', 'Base64 encode files. Into less file format.', function() {
     var data = this.data,
     done = this.async(),
     cwd = './',
-    filesToProcess = grunt.file.expand({cwd: cwd},data.process),
+    filesToProcess = grunt.file.expand({cwd: cwd},data.process);
+    
     output = "";
 
     if(!data.prefix){
@@ -46,44 +73,52 @@ module.exports = function(grunt) {
     }else{
 	grunt.log.writeln("Processing  "+filesToProcess.length+" files...");
 	for(var i = 0; i < filesToProcess.length; i++){
+		var lessFileName = filesToProcess[i].substring(filesToProcess[i].lastIndexOf("/")+1),
+		    lessSafeFileName = lessFileName.replace(".","_"),
+		    fileName = filesToProcess[i];
 		if(data.resize){
-			var dimensions = sizer(filesToProcess[i]);
+			var dimensions = sizer(fileName);
+			function doneCallback(error){
+				pending.push(lessSafeFileName);
+				errorResize(error,data.prefix,lessSafeFileName,fileName,data.dimensions);
+			}
 			if(data.resize.width && !data.resize.height){
 				if(dimensions.width !== data.resize.width){
-					gm(filesToProcess[i]).resize(data.resize.width).write(filesToProcess[i],errorResize);
-					grunt.log.writeln('Resizing file '+filesToProcess[i]+' to width '+data.resize.width);
+					gm(fileName).resize(data.resize.width).write(fileName,doneCallback);
+					grunt.log.writeln('Resizing file '+fileName+' to width '+data.resize.width);
+				}else{
+					output += create(data.prefix,lessSafeFileName,fileName,data.dimensions);
 				}
 			}else if(!data.resize.width && data.resize.height){
 				if(dimensions.height !== data.resize.height){
-					gm(filesToProcess[i]).resize(null,data.resize.height).write(filesToProcess[i],errorResize);
-					grunt.log.writeln('Resizing file '+filesToProcess[i]+' to height '+data.resize.height);
+					gm(fileName).resize(null,data.resize.height).write(fileName,doneCallback);
+					grunt.log.writeln('Resizing file '+fileName+' to height '+data.resize.height);
+				}else{
+					output += create(data.prefix,lessSafeFileName,fileName,data.dimensions);
 				}
 			}else if(data.resize.width && data.resize.height){
 				if(dimensions.width !== data.resize.width && dimensions.height !== data.resize.height){
 					if(data.resize.force){
-						gm(filesToProcess[i]).resize(data.resize.width,data.resize.height,"!").write(filesToProcess[i],errorResize);
+						gm(fileName).resize(data.resize.width,data.resize.height,"!").write(fileName,doneCallback);
 					}else{
-						gm(filesToProcess[i]).resize(data.resize.width,data.resize.height).write(filesToProcess[i],errorResize);
+						gm(fileName).resize(data.resize.width,data.resize.height).write(fileName,doneCallback);
 					}
-					grunt.log.writeln('Resizing file '+filesToProcess[i]+' to width '+data.resize.width+' height '+data.resize.height+' force: '+data.resize.force);
+					grunt.log.writeln('Resizing file '+fileName+' to width '+data.resize.width+' height '+data.resize.height+' force: '+data.resize.force);
+				}else{
+					output += create(data.prefix,lessSafeFileName,fileName,data.dimensions);
 				}
 			}
+		}else{
+			output += create(data.prefix,lessSafeFileName,fileName,data.dimensions);
 		}
-		var lessFileName = filesToProcess[i].substring(filesToProcess[i].lastIndexOf("/")+1),
-		lessSafeFileName = lessFileName.replace(".","_"),
-		contents = fs.readFileSync(filesToProcess[i]),
-		base64Contents = contents.toString('base64');
-		
-		output += "@"+data.prefix+lessSafeFileName+":\"data:"+mime.lookup(filesToProcess[i])+";base64,"+base64Contents+"\";\n";
-	
-		if(data.dimensions){
-			var dimensions1 = sizer(filesToProcess[i]);
-			output += "@"+data.prefix+lessSafeFileName+"_width: "+dimensions1.width+"px;\n";
-			output += "@"+data.prefix+lessSafeFileName+"_height: "+dimensions1.height+"px;\n";	
-		}
-		grunt.log.writeln('Base64 encoded "' + filesToProcess[i] + '" to '+data.prefix+lessSafeFileName+' less entry.');    
 	}
-	grunt.file.write(data.dest, output);
+	
+	completeTimer = setInterval(function checkIfDone(){
+		if(pending.length === 0){
+			grunt.file.write(data.dest, output);	
+			clearInterval(completeTimer);
+		}
+	}, 1000);
 
     }
   });
